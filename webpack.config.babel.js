dad81f683158983a9060f91ef3ef17a4121bf21a
Adil Nurtayev <anurtayev@yahoo.com>
2016-10-07 21:12:57 -0400

    first commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,34 @@
+import webpack from 'webpack'
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.js'
+    ],
+
+    output: {
+        path: './dist',
+        filename: 'bundle.js',
+        publicPath: 'http://localhost:8080/'
+    },
+
+    module: {
+        loaders: [{
+            test: /\.js/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }]
+    },
+
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.IgnorePlugin(/most-adapter/)
+    ],
+
+    devServer: {
+        hot: true,
+        contentBase: './dist'
+    }
+}
