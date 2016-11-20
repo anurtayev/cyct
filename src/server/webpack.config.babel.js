5ecdfb7b820396f3c19f4e5ed9556447eda9e44b
Adil Nurtayev <anurtayev@yahoo.com>
2016-11-20 11:38:47 -0500

    webpack devtool

--- src/server/webpack.config.babel.js
+++ src/server/webpack.config.babel.js
@@ -2,32 +2,32 @@ import webpack from 'webpack'
 import path from 'path'
 
 module.exports = {
-	entry: [
-		'webpack-hot-middleware/client',
-		'./src/client/app.js'
-	],
+    entry: [
+        'webpack-hot-middleware/client',
+        './src/client/app.js'
+    ],
 
-	output: {
-		path: path.join(process.cwd(), 'public'),
-		filename: 'bundle.js',
-		publicPath: '/'
-	},
+    output: {
+        path: path.join(process.cwd(), 'public'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
 
-	module: {
-		loaders: [{
-				test: /\.js/,
-				loader: 'babel',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				loader: 'style!css'
-			}
-		]
-	},
+    module: {
+        loaders: [{
+            test: /\.js/,
+            loader: 'babel',
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            loader: 'style!css'
+        }]
+    },
 
-	plugins: [
-		new webpack.NoErrorsPlugin(),
-		new webpack.HotModuleReplacementPlugin()
-	]
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    devtool: 'eval-source-map'
 }
